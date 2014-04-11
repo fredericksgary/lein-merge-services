b9f5e4bf380b7ccda142616259bcc57f526beb0e
Gary Fredericks <fredericksgary@gmail.com>
2014-04-11 08:57:41 -0500

    I think the .* is implicit in that regex.

--- src/lein_merge_services/plugin.clj
+++ src/lein_merge_services/plugin.clj
@@ -2,7 +2,7 @@
 
 (defn middleware
   [project]
-  (assoc-in project [:uberjar-merge-with #"^META-INF/services/.*"]
+  (assoc-in project [:uberjar-merge-with #"^META-INF/services/"]
             '[#(-> % (slurp) (clojure.string/trim) (str \newline))
               str
               spit]))
